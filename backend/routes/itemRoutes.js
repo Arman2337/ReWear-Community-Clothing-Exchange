8245ce2661d20fe093a999774d4097feb9738d0f
Arman2337 <23it119@charusat.edu.in>
2025-07-12 15:25:52 +0530

    update itemRoutes.js

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/upload');
-const { addItem, getAllItems, getItemById } = require('../controllers/itemController');
+const { addItem, getAllItems, getItemById , getFeaturedItems, getLatestItems,
+  getAllCategories, } = require('../controllers/itemController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, upload.single('image'), addItem);
 router.get('/', getAllItems);
+router.get('/featured', getFeaturedItems);
+router.get('/latest', getLatestItems);
+router.get('/categories', getAllCategories);
 router.get('/:id', getItemById);
-
 module.exports = router;
