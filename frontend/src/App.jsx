41cc095843a15c35e921920b248187d4e6a0c094
Arman2337 <23it119@charusat.edu.in>
2025-07-12 11:34:31 +0530

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import LandingPage from "./LandingPage";
-import ItemDetailPage from "./ItemDetailPage";
-import UserDashboard from "./UserDashboard";
-import ProductDetail from "./ProductDetail";
-import AdminPanel from "./AdminPanel";
-import BrowseItems from "./BrowseItems";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import LandingPage from "./pages/LandingPage";
+import ItemDetailPage from "./pages/ItemDetailPage";
+import UserDashboard from "./pages/UserDashboard";
+import ProductDetail from "./pages/ProductDetail";
+import AdminPanel from "./pages/AdminPanel";
+import BrowseItems from "./pages/BrowseItems";
+// import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <Routes>
-         <Route path="/" element={<LandingPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/item/:id" element={<ItemDetailPage />} />   
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/product/:id" element={<ProductDetail />} />     
-        <Route path="/admin" element={<AdminPanel />} />    
-        <Route path="/browse" element={<BrowseItems />} />
+ 
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/item/:id" element={<ItemDetailPage />} />
+            <Route path="/dashboard" element={<UserDashboard />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/browse" element={<BrowseItems />} />
       </Routes>
     </Router>
   );
